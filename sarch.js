f3e89ad9eb2ee32c612ffd3c3f9a1d158b49204e
smanaman <sanvatsman@gmail.com>
2024-10-22 15:55:35 -0700

    abhi bus

---
+++ sarch.js
@@ -0,0 +1,139 @@
+function Hyderabad() {
+    let city = document.getElementById('city')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}
+
+// city two
+function Bangalore() {
+    let city = document.getElementById('cityTwo')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}
+
+// city three
+function Chennai() {
+    let city = document.getElementById('cityThree')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}// city four
+function Goa() {
+    let city = document.getElementById('cityFour')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}// city five
+function Vijayawada() {
+    let city = document.getElementById('cityFive')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}// city six
+function Nellore() {
+    let city = document.getElementById('citySix')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}// city seven
+function Jaipur() {
+    let city = document.getElementById('citySeven')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}// city eight
+function Mangalore() {
+    let city = document.getElementById('cityEight')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}// city nine
+function Visakhapatnam() {
+    let city = document.getElementById('cityNine')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}
+// city ten
+function Delhi() {
+    let city = document.getElementById('cityTen')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}
+//city 13
+function Indore() {
+    let city = document.getElementById('city11')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}
+//city 12
+function Kolkata() {
+    let city = document.getElementById('city12')
+
+    let citytext = city.textContent
+
+    let input = document.querySelector('.input_tag')
+
+    let inp = input.setAttribute('value', citytext)
+
+
+}
